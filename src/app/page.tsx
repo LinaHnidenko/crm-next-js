a16022fb92c92c821feb42b947e6c7c007c49328
LinaHnidenko <hnidenko99@gmail.com>
2024-03-05 23:26:11 +0200

    practice

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import StatusLabel, { Status } from '@/app/components/status-label';
+import { headers } from 'next/headers';
+
+export default function Home() {
+  console.log(headers());
+  return (
+    <main>
+      <h1 className="text-5xl text-black underline">
+        Home page {new Date().toTimeString()}
+      </h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+    </main>
+  );
+}
